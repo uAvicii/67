16cfe713dc9636f4fbb1c28579059d23da642734
pepe <pepe2077@163.com>
2023-12-20 16:39:20 +0800

    push

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/2077',
+  base: '/test-build/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
