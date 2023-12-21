87c12973a2c69794b3bb5a9b7eb7f9174f68dc36
pepe <pepe2077@163.com>
2023-12-21 13:56:33 +0800

    Updates

--- assets/rules-1f659873.js
+++
@@ -1,1 +0,0 @@
-const e=[{required:!0,message:"请输入手机号"},{pattern:/^1[3-9]\d{9}$/,message:"手机号格式不正确"}],s=[{required:!0,message:"请输入密码"},{pattern:/^\w{8,24}$/,message:"密码需8-24个字符"}];export{e as m,s as p};
